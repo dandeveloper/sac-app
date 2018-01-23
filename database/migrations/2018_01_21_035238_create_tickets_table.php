40c85137060e55e7b30220a4e3c5c0f403866d1a
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-01-23 19:19:25 -0200

    Removed unecessary User references columns

--- database/migrations/2018_01_21_035238_create_tickets_table.php
+++ database/migrations/2018_01_21_035238_create_tickets_table.php
@@ -16,8 +16,6 @@ class CreateTicketsTable extends Migration
         Schema::create('tickets', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
             $table->enum('type',
             [
                 'Telefone',
