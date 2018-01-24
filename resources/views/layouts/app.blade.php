a271f48474cb1169dba9c379f56703789c27d97e
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-01-24 02:50:50 -0200

    Fixing App Layout view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,23 +34,19 @@
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
+                            {{--  <li><a href="{{ route('login') }}">Login</a></li>  --}}
                             <li><a href="{{ route('register') }}">Registrar Atendente</a></li>
                         @else
+                            <li><a href="{{ route('ticket.index') }}">Listar Chamados</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
-
+                                
                                 <ul class="dropdown-menu">
                                     <li>
                                         <a href="{{ route('logout') }}"
