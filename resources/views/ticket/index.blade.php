c9f42e27245277cfc8cd3700c0e640ee2dad85a4
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-01-24 02:51:17 -0200

    Adding Date to index view

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -3,13 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row">
+        <a href="{{ route('ticket.create') }}" class="btn btn-success pull-right">Novo Chamado</a>
         <table class="table table-striped">
             <thead class="thead-dark">
                 <tr>
-                    <th>ID do Chamado</th>
+                    <th>Data do Chamado</th>
                     <th>Data</th>
                     <th>Estado</th>
                     <th>Motivo</th>
+                    <th></th>
                     <th></th>
                     <th></th>
                 </tr>
@@ -17,10 +19,11 @@
             <tbody>
                 @foreach ($tickets as $ticket)
                     <tr>
-                        <td>{{ $ticket->id }}</td>
+                        <td>{{ date('d-m-Y', strtotime($ticket->created_at)) }}</td>
                         <td>{{ $ticket->type }}</td>                        
                         <td>{{ $ticket->state }}</td>
                         <td>{{ $ticket->subject }}</td>
+                        <td><a href="{{ action('TicketController@show', $ticket['id']) }}"><i class="glyphicon glyphicon-eye-open"></i> Visualizar Chamado</a></td>
                         <td><a href="{{ action('TicketController@edit', $ticket['id']) }}"><i class="glyphicon glyphicon-edit"></i> Editar Chamado</a></td>
                         <td>
                             <form action="{{action('TicketController@destroy', $ticket['id'])}}" method="post">
